76bda733390bc3dae370f2574e64229d64dda510
Branden Chan <branden.chan@deepset.ai>
2019-04-16 15:51:07 +0200

    removed unused import

--- run_pretraining.py
+++ run_pretraining.py
@@ -24,8 +24,6 @@ import optimization
 import tensorflow as tf
 from knockknock import slack_sender
 
-from official.utils.logs import hooks_helper
-
 webhook_url="https://hooks.slack.com/services/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"
 
 flags = tf.flags
