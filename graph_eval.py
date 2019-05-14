62304007de8db076e3c68a506259bcefb0ad8595
Branden Chan <brandenchan@icloud.com>
2019-05-14 12:23:50 +0200

    graph eval now saves plots

--- graph_eval.py
+++ graph_eval.py
@@ -1,15 +1,39 @@
 
-#%%
 import pysnooper
 from pprint import pprint
+import argparse
+import matplotlib.pyplot as plt
 
-#%%
-EVAL_FILE = "eval_results.txt"
-lines = [l[:-1] for l in open(EVAL_FILE)]
-grouped_lines = [lines[x:x+9] for x in range(0, len(lines), 9)]
-pprint(grouped_lines)
 
-#%%
+def graph(eval_file):
+
+    lines = [l[:-1] for l in open(eval_file)]
+    grouped_lines = [lines[x:x+9] for x in range(0, len(lines), 9)]
+    parsed_groups = [parse_group(gl) for gl in grouped_lines]
+    
+    losses = ["loss", "masked_lm_loss", "next_sentence_loss"]
+    accuracy = ["masked_lm_accuracy", "next_sentence_accuracy"]
+
+    for a in accuracy:
+        coords = [(int(pg["global_step"]), float(pg[a])) for pg in parsed_groups]
+        coords = sorted(coords, key=lambda x: x[0])
+        x, y = zip(*coords)
+        plt.plot(x, y, label=a)
+    plt.legend()
+    save_file = eval_file[:-4] + "_accuracy.png"
+    plt.savefig(save_file)
+    plt.clf()
+
+    for l in losses:
+        coords = [(int(pg["global_step"]), float(pg[l])) for pg in parsed_groups]
+        coords = sorted(coords, key=lambda x: x[0])
+        x, y = zip(*coords)
+        plt.plot(x, y, label=l)
+    plt.legend()
+    save_file = eval_file[:-4] + "_loss.png"
+    plt.savefig(save_file)
+    plt.clf()
+
 def parse_group(group):
     stats = {}
     for line in group:
@@ -21,42 +45,10 @@ def parse_group(group):
     stats["global_step"] = int(group[0])
     return stats
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--eval_file', type=str, action="store",
+                        help='txt file where eval results are saved')
+    args = parser.parse_args()
 
-#%%
-parsed_groups = [parse_group(gl) for gl in grouped_lines]
-pprint(parsed_groups)
-
-#%%
-import matplotlib.pyplot as plt
-losses = ["loss", "masked_lm_loss", "next_sentence_loss"]
-accuracy = ["masked_lm_accuracy", "next_sentence_accuracy"]
-
-for a in accuracy:
-    coords = [(int(pg["global_step"]), float(pg[a])) for pg in parsed_groups]
-    coords = sorted(coords, key=lambda x: x[0])
-    x, y = zip(*coords)
-    print(x)
-    print(y)
-    plt.plot(x, y, label=a)
-plt.legend()
-plt.show()
-plt.clf()
-
-for l in losses:
-    coords = [(int(pg["global_step"]), float(pg[l])) for pg in parsed_groups]
-    coords = sorted(coords, key=lambda x: x[0])
-    x, y = zip(*coords)
-    print(x)
-    print(y)
-    plt.plot(x, y, label=l)
-plt.legend()
-plt.show()
-plt.clf()
-
-
-
-
-#%%
-
-
-#%%
+    graph(args.eval_file)
